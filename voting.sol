f5417d569a82b2004b8c1fabdb41d539b22b29d8
Vinicius Macelai <vinicius.macelai@gmail.com>
2018-11-25 03:41:19 +0000

    struct for candidate, using maps

--- voting.sol
+++ voting.sol
@@ -2,40 +2,48 @@ pragma solidity ^0.5.0;
 
 contract Voting {
     
-    bytes32[] candidateList;
-    mapping (bytes32 => uint32) votesReceived;
+    struct Candidate {
+        bytes32 name;
+        uint8 party;
+        uint32 voteCount;
+    }
+
+    mapping (uint8 => Candidate) candidates;
     mapping (address => uint8) whiteList;
     uint initTime;
     uint32 durationTimeMin;
     
-    constructor(uint32 _durationTimeMin, bytes32[] memory _candidateList ,address[] memory _adresses) public {
-        candidateList = _candidateList;
+    constructor(uint32 _durationTimeMin) public {
         initTime = now;
         durationTimeMin = _durationTimeMin;
-        for (uint i = 0; i < _adresses.length; i++) {
-            whiteList[_adresses[i]] = 1;
-        }
+    }
+    
+    function addToWhiteList(address _address) public{
+        whiteList[_address] = 1;
+    }
+    
+    function addCandidate(bytes32 _name, uint8 _party) public {
+        Candidate memory newCandidate = Candidate({name: _name, party: _party, voteCount: 0}); 
+        candidates[newCandidate.party] = newCandidate;
     }
 
-    function totalVotesFor(bytes32 candidate) public view returns (uint32){
+    function totalVotesFor(uint8 candidate) public view returns (uint32){
         require(validCandidate(candidate));
-        return votesReceived[candidate];
+        return candidates[candidate].voteCount;
     }
 
-    function voteForCandidate(bytes32 candidate) public {
+    function voteForCandidate(uint8 candidate) public {
         require(validCandidate(candidate));
         require(validVoter(msg.sender));
         require(validTime());
-        votesReceived[candidate] += 1;
+        candidates[candidate].voteCount ++;
         whiteList[msg.sender] = 2;
     }
 
-    function validCandidate(bytes32 candidate) public view returns (bool) {
-        for (uint i = 0; i < candidateList.length; i++) {
-        if (candidateList[i] == candidate) {
+    function validCandidate(uint8 candidate) public view returns (bool) {
+        if (candidates[candidate].party == candidate) {
             return true;
-          }
-    }
+        }
         return false;
     }
     
@@ -52,9 +60,6 @@ contract Voting {
         }
         return false;
     }
-
-    function getCandidateList() public view returns (bytes32[] memory) {
-        return candidateList;
-    }
+    
 }
 
