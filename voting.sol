191dd843377eb169f8a3981fa8a6f931cebea600
Vinicius Macelai <vinicius.macelai@gmail.com>
2018-11-26 00:27:50 +0000

    Add possibility to vote more than one time and more verifications

--- voting.sol
+++ voting.sol
@@ -4,43 +4,60 @@ contract Voting {
     
     struct Candidate {
         bytes32 name;
-        uint8 party;
+        uint16 party;
         uint32 voteCount;
     }
 
-    mapping (uint8 => Candidate) candidates;
-    mapping (address => uint8) whiteList;
+    mapping (uint16 => Candidate) candidates;
+    mapping (address => uint16) whiteList;
+    // Timestamp
     uint initTime;
-    uint32 durationTimeMin;
+    uint durationTimeMin;
     
-    constructor(uint32 _durationTimeMin) public {
-        initTime = now;
-        durationTimeMin = _durationTimeMin;
+    function setTime(uint _initTime, uint _durationTime) public {
+        initTime = _initTime;
+        durationTimeMin = _durationTime;
     }
     
-    function addToWhiteList(address _address) public{
-        whiteList[_address] = 1;
+    function addToWhiteList(address _address) public {
+        require(validTimeToAdd());
+        // To know it's first time voting
+        whiteList[_address] = 999;
     }
     
     function addCandidate(bytes32 _name, uint8 _party) public {
+        require(validTimeToAdd());
+        require(0 < _party);
+        require(100 > _party);
         Candidate memory newCandidate = Candidate({name: _name, party: _party, voteCount: 0}); 
         candidates[newCandidate.party] = newCandidate;
     }
 
-    function totalVotesFor(uint8 candidate) public view returns (uint32){
+    function totalVotesFor(uint16 candidate) public view returns (uint32){
         require(validCandidate(candidate));
         return candidates[candidate].voteCount;
     }
 
-    function voteForCandidate(uint8 candidate) public {
+    function voteForCandidate(uint16 candidate) public {
         require(validCandidate(candidate));
         require(validVoter(msg.sender));
         require(validTime());
-        candidates[candidate].voteCount ++;
-        whiteList[msg.sender] = 2;
+        // Not the first time voting
+        if (whiteList[msg.sender] != 999) {
+            candidates[whiteList[msg.sender]].voteCount--;
+        }
+        candidates[candidate].voteCount++;
+        whiteList[msg.sender] = candidate;
     }
-
-    function validCandidate(uint8 candidate) public view returns (bool) {
+    
+    function validTimeToAdd() public view returns (bool) {
+        if (initTime >= now) {
+            return true;
+        }
+        return false;
+    }
+ 
+    function validCandidate(uint16 candidate) public view returns (bool) {
         if (candidates[candidate].party == candidate) {
             return true;
         }
@@ -48,14 +65,14 @@ contract Voting {
     }
     
     function validVoter(address voter) public view returns (bool) {
-        if (whiteList[voter] == 1) {
+        if (whiteList[voter] != 0) {
             return true;
         }
         return false;
     }
     
     function validTime() public view returns (bool) {
-        if (now < initTime + durationTimeMin * 1 minutes) {
+        if (now < initTime + durationTimeMin) {
             return true;
         }
         return false;
