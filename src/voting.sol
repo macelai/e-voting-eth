c4806c75e6b8feca45236d0edad94dfc712f8483
Vinicius Macelai <vinicius.macelai@gmail.com>
2018-11-29 00:01:17 +0000

    Add onlyowner modifier

--- src/voting.sol
+++ src/voting.sol
@@ -1,35 +1,43 @@
 pragma solidity ^0.5.0;
 
 contract Voting {
-
+    
+    address private owner;
+    
     struct Candidate {
-        string name;
+        bytes32 name;
         uint16 party;
         uint32 voteCount;
     }
 
-    mapping (uint16 => Candidate) candidates;
-    mapping (address => uint16) whiteList;
+    mapping (uint16 => Candidate) public candidates;
+    mapping (address => uint16) public whiteList;
     // Timestamp
-    uint initTime;
-    uint durationTimeMin;
-
-    function setTime(uint _initTime, uint _durationTime) public {
+    uint public initTime;
+    uint public durationTimeMin;
+    
+    constructor(uint _initTime, uint _durationTime) public {
+        owner = msg.sender;
         initTime = _initTime;
         durationTimeMin = _durationTime;
     }
-
-    function addToWhiteList(address _address) public {
+    
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+    
+    function addToWhiteList(address _address) onlyOwner public {
         require(validTimeToAdd());
         // To know it's first time voting
         whiteList[_address] = 999;
     }
-
-    function addCandidate(string memory _name, uint8 _party) public {
+    
+    function addCandidate(bytes32 _name, uint8 _party) onlyOwner public {
         require(validTimeToAdd());
         require(0 < _party);
         require(100 > _party);
-        Candidate memory newCandidate = Candidate({name: _name, party: _party, voteCount: 0});
+        Candidate memory newCandidate = Candidate({name: _name, party: _party, voteCount: 0}); 
         candidates[newCandidate.party] = newCandidate;
     }
 
@@ -41,7 +49,7 @@ contract Voting {
     function voteForCandidate(uint16 candidate) public {
         require(validCandidate(candidate));
         require(validVoter(msg.sender));
-        require(validTimeToVote());
+        require(validTime());
         // Not the first time voting
         if (whiteList[msg.sender] != 999) {
             candidates[whiteList[msg.sender]].voteCount--;
@@ -49,33 +57,34 @@ contract Voting {
         candidates[candidate].voteCount++;
         whiteList[msg.sender] = candidate;
     }
-
+    
     function validTimeToAdd() public view returns (bool) {
         if (initTime >= now) {
             return true;
         }
         return false;
     }
-
+ 
     function validCandidate(uint16 candidate) public view returns (bool) {
         if (candidates[candidate].party == candidate) {
             return true;
         }
         return false;
     }
-
+    
     function validVoter(address voter) public view returns (bool) {
         if (whiteList[voter] != 0) {
             return true;
         }
         return false;
     }
-
-    function validTimeToVote() public view returns (bool) {
-        if (now <= durationTimeMin && now >= initTime) {
+    
+    function validTime() public view returns (bool) {
+        if (now < initTime + durationTimeMin) {
             return true;
         }
         return false;
     }
-
+    
 }
+
